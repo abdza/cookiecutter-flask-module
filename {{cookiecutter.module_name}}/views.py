4687edd43186b8f780d503cacbee148a62e0477b
abdza <abdullah.zainul@gmail.com>
2017-03-05 19:22:35 +0800

    Fix

--- {{cookiecutter.module_name}}/views.py
+++ {{cookiecutter.module_name}}/views.py
@@ -2,7 +2,6 @@
 """Incident views."""
 from flask import Blueprint, render_template, jsonify
 from flask_login import login_required
-from flask_googlemaps import Map
 from sqlalchemy import func
 from {{cookiecutter.app_name}}.extensions import db
 
