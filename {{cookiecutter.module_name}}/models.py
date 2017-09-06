20bfd93c61e4c35753808d60adf647837bc6beff
abdza <abdullah.zainul@gmail.com>
2017-09-06 09:21:58 +0800

    Updated import path

--- {{cookiecutter.module_name}}/models.py
+++ {{cookiecutter.module_name}}/models.py
@@ -3,16 +3,7 @@
 import datetime as dt
 
 from flask import current_app
-from flask_login import UserMixin
-from {{cookiecutter.app_name}}.database import Column, Model, SurrogatePK, db, reference_col, relationship
-from {{cookiecutter.app_name}}.extensions import bcrypt
-from {{cookiecutter.app_name}}.settings import ProdConfig
-
-import hashlib
-import hmac
-import base64
-import urlparse
-import urllib
+from ..database import Column, Model, SurrogatePK, db, reference_col, relationship
 
 class {{cookiecutter.module_name|title}}(SurrogatePK,Model):
     __tablename__ = '{{cookiecutter.module_name}}'
--- {{cookiecutter.module_name}}/views.py
+++ {{cookiecutter.module_name}}/views.py
@@ -3,6 +3,6 @@
 from flask import Blueprint, render_template, jsonify
 from flask_login import login_required
 from sqlalchemy import func
-from {{cookiecutter.app_name}}.extensions import db
+from ..extensions import db
 
 blueprint = Blueprint('{{cookiecutter.module_name}}', __name__, url_prefix='/{{cookiecutter.module_name}}', static_folder='../static')
